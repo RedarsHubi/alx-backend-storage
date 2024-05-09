2c93e1cf3404fb830f8b81823c6a781c9c0b26a4
RedarsHubi <redamasters@gmail.com>
2024-05-09 11:39:17 +0100

    ?

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,7 +3,8 @@ CREATE TRIGGER validate
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 0;
-    END IF;
-END;
+IF NEW.email <> OLD.email
+THEN
+    SET NEW.valid_email = 0;
+END IF;
+END
