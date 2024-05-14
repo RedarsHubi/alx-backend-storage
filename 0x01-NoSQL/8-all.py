526dff72e2630d14f527ca35e77cd7092395225a
RedarsHubi <redamasters@gmail.com>
2024-05-14 12:18:39 +0100

    pyco

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ lists all documents in a collection """
 
+
 def list_all(mongo_collection):
     """ returns list of collection """
     return [document for document in mongo_collection.find()]
