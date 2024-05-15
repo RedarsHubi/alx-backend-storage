90b24c4e0403fc3940242eeb3eaa5717e354398c
RedarsHubi <redamasters@gmail.com>
2024-05-15 15:18:29 +0100

    Implement count for cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,17 @@ Cache module
 import redis
 import uuid
 from typing import Union, Callable, Optional
+import functools
 
+
+def count_calls(method: Callable) -> Callable:
+    """counts the number of times a method is called."""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = f"count:{method.__qualname__}"
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     def __init__(self):
@@ -13,11 +23,13 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ takes a data argument and returns a string """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
     def get(self, key: str, fn: Callable = None) ->  Union[bytes, None]:
         """ used to convert the data back to the desired format """
         value = self._redis.get(key)
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,32 +1,17 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
+""" Main file """
 
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+key_first = cache.store(b"first")
+print(cache.get(key_first))  # Retrieve the data associated with the key
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+# Store more data
+key_second = cache.store(b"second")
+key_third = cache.store(b"third")
 
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    print(f"Storing value: {value}")
-    key = cache.store(value)
-    print(f"Retrieved key: {key}")
-    retrieved_value = cache.get(key, fn=fn)
-    print(f"Retrieved value: {retrieved_value}")
-    assert retrieved_value == value
+# Retrieve data using the correct keys
+print(cache.get(key_second))  # Retrieve the data associated with the key generated for "second"
+print(cache.get(key_third))   
