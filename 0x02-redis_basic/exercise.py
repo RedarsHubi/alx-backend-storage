cd051f86141d8fbb3c82cea96ede62698e1cfb1f
RedarsHubi <redamasters@gmail.com>
2024-05-15 14:47:40 +0100

    Converting data using callable

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Cache module
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,3 +18,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    def get(self, key: str, fn: Callable = None) ->  Union[bytes, None]:
+        """ used to convert the data back to the desired format """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+    def get_str(self, key: str) -> Optional[str]:
+        """parametrizes Cache.get with the correct conversion function"""
+        return self.get(key, lambda x: x.decode('utf-8') if x else None)
+
+    def get_int(self, key: int) -> Optional[int]:
+        """parametrizes Cache.get with the correct conversion function"""
+        return self.get(key, fn=int)
+
+        
+
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -14,3 +14,19 @@ print(key)
 
 local_redis = redis.Redis()
 print(local_redis.get(key))
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    print(f"Storing value: {value}")
+    key = cache.store(value)
+    print(f"Retrieved key: {key}")
+    retrieved_value = cache.get(key, fn=fn)
+    print(f"Retrieved value: {retrieved_value}")
+    assert retrieved_value == value
